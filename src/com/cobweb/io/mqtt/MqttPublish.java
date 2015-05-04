72b4b1ead731407141c345bfbce4522761a85649
yasith <yasith@yasith-laptop>
2015-05-04 19:47:56 +0530

    MQTT publisher added

---
+++ src/com/cobweb/io/mqtt/MqttPublish.java
@@ -0,0 +1,44 @@
+package com.cobweb.io.mqtt;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MqttPublish {
+
+	public static void main(String[] args) {
+
+		String topic = "MQTT";
+		String content = "HelloWorld";
+		int qos = 2;
+		String brokerUrl = "tcp://localhost:1883";
+		String clientId = "001";
+		MemoryPersistence persistence = new MemoryPersistence();
+
+		try {
+			MqttClient sampleClient = new MqttClient(brokerUrl, clientId, persistence);
+			MqttConnectOptions connOpts = new MqttConnectOptions();
+			connOpts.setCleanSession(true);
+			System.out.println("Connecting to broker: " + brokerUrl);
+			sampleClient.connect(connOpts);
+			System.out.println("Connected");
+			System.out.println("Publishing message: " + content);
+			MqttMessage message = new MqttMessage(content.getBytes());
+			message.setQos(qos);
+			sampleClient.publish(topic, message);
+			System.out.println("Message published");
+			sampleClient.disconnect();
+			System.out.println("Disconnected");
+			System.exit(0);
+		} catch (MqttException me) {
+			System.out.println("reason " + me.getReasonCode());
+			System.out.println("msg " + me.getMessage());
+			System.out.println("loc " + me.getLocalizedMessage());
+			System.out.println("cause " + me.getCause());
+			System.out.println("excep " + me);
+			me.printStackTrace();
+		}
+	}
+}
