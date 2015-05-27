445c9fb7436792723dc5efd24a0cb5b4b703005d
YasithLokuge <yasith1@gmail.com>
2015-05-27 16:38:25 +0530

    shiro changes added

--- src/com/cobweb/io/api/RestDemo.java
+++ src/com/cobweb/io/api/RestDemo.java
@@ -1,22 +1,32 @@
 package com.cobweb.io.api;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+
 @Path("/api")
 public class RestDemo {
 
-	 	@POST
-	    @Path("/post")
-	    @Consumes(MediaType.TEXT_PLAIN)
-	    public Response createDataInJSON(String data) { 
-
-	        String result = "Data post: "+data;
-
-	        System.out.println(data);
-	        return Response.status(201).entity(result).build(); 
-	    }
+//	 	@POST
+//	    @Path("/post")	 
+//	    @Consumes(MediaType.TEXT_PLAIN)
+//	 	@RequiresAuthentication
+//	    public Response createDataInJSON(String data) { 
+//
+//	        String result = "Data post: "+data;
+//
+//	        System.out.println(data);
+//	        return Response.status(201).entity(result).build(); 
+//	    }
+	
+	
+	@GET
+	public String get() {
+        return "OK";
+    }
 }
