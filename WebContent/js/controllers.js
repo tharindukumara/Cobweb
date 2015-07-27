3b7c49dec8277f9e6165ede76293a797f525480c
YasithLokuge <yasith1@gmail.com>
2015-07-27 12:18:48 +0530

    frontend changes added

--- WebContent/js/controllers.js
+++ WebContent/js/controllers.js
@@ -4,4 +4,7 @@ myApp.controller('MyController', function MyController($scope, $http) {
     $http.get('http://localhost:8080/cobweb/api/newsfeed/sensor').success(function(data) {
         $scope.sensorNewsfeed = data;
     });
+    $http.get('http://localhost:8080/cobweb/api/newsfeed/device').success(function(data) {
+        $scope.deviceNewsfeed = data;
+    });
 })
