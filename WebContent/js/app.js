8e9ba07e823934b847876c1b61ae2d7c1a53f334
Malith Senaweera <malithsenaweera@gmail.com>
2015-07-29 22:42:35 +0530

    Angular routing. WIP.

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('CobWebApp', ['ngMaterial']);
+var app = angular.module('CobWebApp', ['ngMaterial', 'ngRoute']);
 
 
 
@@ -8,3 +8,38 @@ app.config(function($mdThemingProvider) {
         .primaryPalette('blue')
         .accentPalette('red');
 });
+
+app.config(function($routeProvider) {
+
+  $routeProvider
+  .when('/news', {
+    templateUrl: 'newsfeed.html',
+    controller: 'CobWebAppCtrl'
+  })
+  .when('/about', {
+    templateUrl: 'views/about',
+    controller: 'MetaCtrl'
+  })
+  .otherwise({
+    templateUrl: 'views/404',
+    controller: 'MetaCtrl'
+  });
+});
+
+app.controller('CobWebAppCtrl', ['$scope', '$mdSidenav', '$http', function($scope, $mdSidenav, $http) {
+  console.log("ctrller fired");
+  $http.get('http://localhost:8080/cobweb/api/newsfeed/device').success(function(data) {
+    $scope.chunks = data;
+  });
+
+}]);
+
+app.controller('LayoutCtrl', ['$scope', '$mdSidenav', '$http', function($scope, $mdSidenav, $http) {
+  $scope.toggleMenu = function() {
+      $mdSidenav('left').toggle();
+  };
+
+  $scope.close = function() {
+    $mdSidenav('left').close();
+  }
+}]);
