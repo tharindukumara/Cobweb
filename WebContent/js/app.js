7aadea13f8ffb48bbdc85a0668d54c3c6ff9bf78
Malith Senaweera <malithsenaweera@gmail.com>
2015-07-31 18:55:56 +0530

    Add loading spinner

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -17,11 +17,23 @@ app.config(function($routeProvider) {
   });
 });
 
-app.controller('CobWebAppCtrl', ['$scope', '$http', function($scope, $http) {
-  console.log("ctrller fired");
-  $http.get('http://localhost:8080/cobweb/api/device/newsfeed').success(function(data) {
-    $scope.chunks = data;
-  });
+app.controller('CobWebAppCtrl', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
+  console.log("cobwerbapp ctrller fired");
+
+  $scope.loadNews = function() {
+    $rootScope.newsLoaded = false;
+    $http.get('http://localhost:8080/cobweb/api/device/newsfeed').success(function(data) {
+      $scope.news = data;
+      $rootScope.newsLoaded = true;
+    });
+  };
+
+  $scope.loadNews();
+
+}]);
+
+app.controller('LayoutCtrl', ['$rootScope', function($rootScope) {
+  console.log("layout ctrller fired");
 
 }]);
 
