4c65899eaeb7fdd1789717583144ab966c119ad2
Malith Senaweera <malithsenaweera@gmail.com>
2015-07-31 09:00:59 +0530

    Changed cards view

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,13 +1,4 @@
-var app = angular.module('CobWebApp', ['ngMaterial', 'ngRoute']);
-
-
-
-//Config theme
-app.config(function($mdThemingProvider) {
-    $mdThemingProvider.theme('blue')
-        .primaryPalette('blue')
-        .accentPalette('red');
-});
+var app = angular.module('CobWebApp', ['ngRoute']);
 
 app.config(function($routeProvider) {
 
@@ -26,7 +17,7 @@ app.config(function($routeProvider) {
   });
 });
 
-app.controller('CobWebAppCtrl', ['$scope', '$mdSidenav', '$http', function($scope, $mdSidenav, $http) {
+app.controller('CobWebAppCtrl', ['$scope', '$http', function($scope, $http) {
   console.log("ctrller fired");
   $http.get('http://localhost:8080/cobweb/api/newsfeed/device').success(function(data) {
     $scope.chunks = data;
@@ -34,12 +25,3 @@ app.controller('CobWebAppCtrl', ['$scope', '$mdSidenav', '$http', function($scop
 
 }]);
 
-app.controller('LayoutCtrl', ['$scope', '$mdSidenav', '$http', function($scope, $mdSidenav, $http) {
-  $scope.toggleMenu = function() {
-      $mdSidenav('left').toggle();
-  };
-
-  $scope.close = function() {
-    $mdSidenav('left').close();
-  }
-}]);
