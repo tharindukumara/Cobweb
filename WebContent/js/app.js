5a5a69af4be09b6e9bfda26655a03ff78d8ea3ca
Malith Senaweera <malithsenaweera@gmail.com>
2015-08-21 17:45:04 +0530

    Items page: Device/Sesor cards

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -462,7 +462,7 @@ app.controller('UserCtrl', ['$scope', '$http', '$routeParams', '$rootScope', 'ng
 
 }]);
 
-app.controller('ItemsCtrl', ['$rootScope', '$scope', '$http', function($rootScope, $scope, $http) {
+app.controller('ItemsCtrl', ['$rootScope', '$scope', '$http', 'ngDialog', function($rootScope, $scope, $http, ngDialog) {
   console.log("Items ctrller fired");
 
   $scope.devices = [];
@@ -584,14 +584,14 @@ app.controller('ItemsCtrl', ['$rootScope', '$scope', '$http', function($rootScop
   $scope.deviceIdlst = [];
   $scope.items = [];
 
-  function loadDeviceList(cb){
+  function loadDevices(cb){
     $http.get('/api/device/subscriptions').success(function(data) {
       $scope.deviceIdlst = data;
       cb(data);
     });
   }
 
-  function loadDeviceData(lst){
+  function loadDeviceInfo(lst){
     lst.forEach(function(id){
       $http.get('/api/device/' + id).success(function(device) {
         var obj = {
@@ -617,8 +617,114 @@ app.controller('ItemsCtrl', ['$rootScope', '$scope', '$http', function($rootScop
         });
       });
     });
+    $scope.loadDeviceList(lst[0].device.name); //Show the details of first device. Find a better way.
     console.log(lst);
   }
+
+  $scope.loadDeviceList = function(deviceName){
+    $scope.deviceVisible = true;
+    $scope.sensorVisible = false;
+    $scope.selectedItem = _.filter($scope.items, {device: {name: deviceName}});
+  }
+
+  $scope.loadSensorList = function(sensorName) {
+    var sensorList = [];
+    $scope.sensorVisible = true;
+    $scope.deviceVisible = false;
+    $scope.selectedItem.forEach(function(device){
+      device.sensors.forEach(function(sensor){
+        sensorList.push(sensor);
+      });
+    });
+    $scope.selectedItem = _.filter(sensorList, {name:sensorName});
+  }
+
+  $scope.loadCards = function(id, type){
+    if (id){
+      if (type == 'device') {
+        loadDeviceCards(id);
+      } else if (type == 'sensor') {
+        loadSensorCards(id);
+      }
+    }
+  }
+
+  function loadDeviceCards(id) {
+    $scope.cardLst = [];
+
+    $http.get('/api/device/message/' + id).success(function(data) {
+      data.forEach(function(device){
+
+        dev = {
+          id: '',
+          name: '',
+          userId: '',
+          time: '',
+          msg: '',
+          item: 'device'
+        };
+
+        dev.id = device.deviceId;
+        dev.userId = device.userId;
+        dev.time = device.dateTime;
+        dev.msg = device.message;
+        $scope.cardLst.push(dev);
+      });
+      console.log($scope.cardLst);
+    });
+  }
+
+  function loadSensorCards(id){
+    $scope.cardLst = [];
+
+    $http.get('/api/sensor/message/' + id).success(function(data) {
+      data.forEach(function(sensor){
+
+        sen = {
+          id: '',
+          deviceId: '',
+          name: '',
+          userId: '',
+          time: '',
+          msg: '',
+          item: 'sensor'
+        };
+
+        sen.id = sensor.sensorId;
+        sen.userId = sensor.userId;
+        sen.deviceId = sensor.deviceId;
+        sen.time = sensor.timeStamp;
+        sen.msg = sensor.message;
+        $scope.cardLst.push(sen);
+      });
+      console.log($scope.cardLst);
+    });
+  }
+
+  function loadDataById(obj, type){
+    $http.get('/api/' + type +'/' + obj.id).success(function(data) {
+      obj.name = data.name;
+      obj.type = data[type+'Type'];
+    });
+  }
+
+  function loadDeviceData(obj){
+    $http.get('/api/device/' + obj.deviceId).success(function(data) {
+      obj.deviceName = data.name;
+      obj.deviceType = data.deviceType;
+    });
+  }
+
+  function loadUserData(obj){
+    $http.get('/api/friends/' + obj.userId).success(function(data) {
+      obj.userName = data.firstName + ' ' + data.lastName;
+      obj.emailHash = data.emailHash;
+    });
+  }
+
+  $scope.popup = function (obj) {
+    ngDialog.open({ template: '<h2>'+obj.userName+'</h2><p>Name: '+ obj.name+'</p>' +'<p>Id: '+ obj.id+'</p>'+'<p>Type: '+ obj.type+'</p>', className: 'ngdialog-theme-default', plain: true});
+  };
 
   $scope.$watch('items.length', function(newval, old){
     if (newval == $scope.deviceIdlst.length){
@@ -626,7 +732,25 @@ app.controller('ItemsCtrl', ['$rootScope', '$scope', '$http', function($rootScop
     }
   }, true);
 
-  loadDeviceList(loadDeviceData);
+  $scope.$watch('cardLst', function(newval, old){
+    console.log(newval);
+    if (newval !== undefined && newval.length !== 0){
+      if (newval[0].item == 'device'){
+        newval.forEach(function(obj){
+          loadDataById(obj, 'device');
+          loadUserData(obj);
+        });
+      } else if (newval[0].item == 'sensor'){
+        newval.forEach(function(obj){
+          loadDataById(obj, 'sensor');
+          loadUserData(obj);
+          loadDeviceData(obj);
+        });
+      }
+    }
+  }, true);
+
+  loadDevices(loadDeviceInfo);
   loadSensorNotifications(loadSensorSubscriberName)
   loadDeviceNotifications(loadDeviceSubscriberName);
 
